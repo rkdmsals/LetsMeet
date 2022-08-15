755aec7110b77b7baa96e02b55854ed1c599d8f5
lucykorea414 <31093793+lucykorea414@users.noreply.github.com>
2022-08-16 01:42:35 +0900

    logout 기능 추가

--- mainweb/sociallogin/urls.py
+++ mainweb/sociallogin/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('sociallogin/', include('allauth.urls')),
     path('', views.sociallogin, name='sociallogin'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- mainweb/sociallogin/views.py
+++ mainweb/sociallogin/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth import logout
 
 def sociallogin(request):
     return render(request, 'sociallogin/sociallogin.html')
+
+
+def logout_view(request):
+    logout(request)
+    return render(request, 'sociallogin/logout.html')
