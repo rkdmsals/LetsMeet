70ace505a922a9a3e6a15a8b9f4ae1533bfc2c86
rkdmsslasla <gaeun9477@gmail.com>
2022-08-22 15:00:21 +0900

    profile모델 수정

--- mainweb/users/admin.py
+++ mainweb/users/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from .models import Profile
 
 # Register your models here.
+
+class ProfileInline(admin.StackedInline): # 유저 밑에 프로필 을 붙여서 보여주려고 이를 상속받음
+    model = Profile
+    con_delete = False                    # 프로필을 아예 없앨 수 없게 하는 속성(한번 만들면 지우는건 이상하니까)
+
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (ProfileInline,)
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
--- mainweb/users/models.py
+++ mainweb/users/models.py
@@ -1,14 +1,9 @@
 from django.db import models
-
-# Create your models here.
+from django.conf import settings
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    user_pk = models.IntegerField(blank=True)
-    email = models.EmailField(max_length=500, blank=True)
-    mygit = models.CharField(max_length=50, blank=True)
-    nickname = models.CharField(max_length=200, blank=True)
-    myInfo = models.CharField(max_length=150, blank=True)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # User모델과 Profile을 1:1로 연결
+    description = models.TextField(blank=True)      # 자기소개 칸 만들까?말까?
+    nickname = models.CharField(max_length=40, blank=True)
