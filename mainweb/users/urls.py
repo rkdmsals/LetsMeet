d02c78a2ca8dbf6a29adc0eeefc67e20cc603f91
lucykorea414 <31093793+lucykorea414@users.noreply.github.com>
2022-08-23 01:33:22 +0900

    minor fix profile

--- mainweb/users/urls.py
+++ mainweb/users/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     # 프로필
     # user_id는 DB상의 인덱스
     # path('profile/base/<int:user_id>/', ProfileView.profile_base, name='profile_base'),
-    path('profile/<int:user_id>/', ProfileView.ProfileCommentListView.as_view(), name='profile'),
+    #path('profile/<int:user_id>/', ProfileView.ProfileCommentListView.as_view(), name='profile'),
 ]
