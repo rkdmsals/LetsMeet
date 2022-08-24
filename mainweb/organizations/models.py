69c0c14615422731e6edc7fbadcac5f866e07ed9
lucykorea414 <31093793+lucykorea414@users.noreply.github.com>
2022-08-24 16:04:26 +0900

    slugify add
    모임별로 slug(url) 자동할당

--- mainweb/organizations/models.py
+++ mainweb/organizations/models.py
@@ -1,23 +1,24 @@
 from django.db import models
 from django.utils import timezone
 from django.urls import reverse
+from django.template.defaultfilters import slugify
 
 # Create your models here.
 
 class Moim(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     detail = models.TextField(null=True)
 
     join_users = models.ManyToManyField('auth.User', verbose_name=u'참여자', blank=True, related_name='moim_users')
     # 모임을 만든 사람이 모임장이 될 수 있게 설정
-    admin_user = models.ForeignKey('auth.User', on_delete=models.CASCADE, null=True) #모임장 삭제되면 모임 삭제
+    admin_user = models.ForeignKey('auth.User', on_delete=models.CASCADE, null=True) #모임장 삭제되면 모임 삭제ㄴ
     #admin_user = models.OneToOneField('auth.User', verbose_name=u'모임장', null=False) 
 
     created_date = models.DateTimeField(default=timezone.now)
     start_date = models.DateField(default=timezone.now)
     end_date = models.DateField(default=timezone.now)
 
-    slug = models.CharField(max_length=255, unique=True, null=True)
+    slug = models.CharField(max_length=100, unique=True, null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -32,3 +33,7 @@ class Moim(models.Model):
         #return reverse('moim-detail-view', args=[str(self.name)])
         #return reverse('', kwargs={"pk": self.id})
         return ('moim-detail-view', (), {'slug': self.slug})
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Moim, self).save(*args, **kwargs)
