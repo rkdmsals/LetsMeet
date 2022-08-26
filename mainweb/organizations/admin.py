f033509bfe9ca1e29f231006241a02da817a69ed
rkdmsslasla <gaeun9477@gmail.com>
2022-08-26 11:34:43 +0900

    코드 합치기 완료!

--- mainweb/organizations/admin.py
+++ mainweb/organizations/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import Moim
 
 class MoimAdmin(admin.ModelAdmin):
-    list_display=['name', 'created_date' ]
-    search_fields=['name']
+    list_display=['moim_name', 'created_date' ]
+    search_fields=['moim_name']
     
 
 admin.site.register(Moim, MoimAdmin)
